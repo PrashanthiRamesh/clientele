90ff850b756b8f0cfa8894ba596c71a9fb8ae3fe
Prashanthi Ramesh <rvprashanthi@gmail.com>
2021-07-30 15:23:21 -0400

    update style

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="clients">
-    <div class="grey--text">Clients</div>
-  </div>
+ <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+        <h1 class="grey--text display-2">Clients</h1>
+    </v-row>
+</v-container>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="dashboard">
-    <div class="grey--text">An awesome CRM coming soon....</div>
-  </div>
+ <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+        <h1 class="grey--text display-2">An awesome CRM coming soon</h1>
+    </v-row>
+</v-container>
 </template>
+
 
 <script>
 export default {
