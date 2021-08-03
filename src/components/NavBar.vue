75f4c1883442d4c5ca5235aef082994fa99a2041
Prashanthi Ramesh <rvprashanthi@gmail.com>
2021-08-03 17:10:35 -0400

    Added simple form

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,23 @@
 <template>
     <nav>
-        <v-app-bar flat app>
+        <v-app-bar text app>
             <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Viji</span>
                 <span>Clientele</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn flat color="grey" dark>
+            <v-btn color="grey" dark>
                 <span>Sign Out</span>
                 <v-icon right>mdi-exit-to-app</v-icon>
             </v-btn>
         </v-app-bar>
         <v-navigation-drawer app class="cyan accent-4" v-model="drawer">
+            <v-layout column align-center>
+                <v-flex class="mt-5 mb-5">
+                   <Popup />
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
@@ -28,7 +33,10 @@
 </template>
 
 <script>
+import Popup from './Popup.vue';
+
 export default {
+    components: { Popup },
     data() {
         return {
             drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-dialog max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn v-bind="attrs" v-on="on" color="blue-grey" class="white--text"
+        >Add new client</v-btn
+      >
+    </template>
+    <v-card>
+      <v-card-title>
+        <div class="grey--text text--darken-2">Add a new client</div>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field
+            label="First Name"
+            v-model="firstName"
+            prepend-icon="mdi-account"
+          ></v-text-field>
+          <v-text-field
+            label="Last Name"
+            v-model="lastName"
+            prepend-icon="mdi-account"
+          ></v-text-field>
+          <v-text-field
+            label="Company"
+            v-model="company"
+            prepend-icon="mdi-domain"
+          ></v-text-field>
+          <v-text-field
+            label="Phone"
+            v-model="phone"
+            prepend-icon="mdi-phone"
+          ></v-text-field>
+          <v-text-field
+            label="Address"
+            v-model="address"
+            prepend-icon="mdi-map-marker"
+          ></v-text-field>
+          <v-btn class="mt-4 green lighten-2 white--text" @click="submit"
+            >Add Client</v-btn
+          >
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      company: "",
+      phone: "",
+      address: "",
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.firstName, this.address);
+    },
+  },
+};
+</script>
